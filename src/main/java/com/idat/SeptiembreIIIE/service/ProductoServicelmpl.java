ca4e66331846259f4846766b97059ded871e6524
Anllelo Josue Yactayo Paz <anllelojosue.y@gmail.com>
2022-10-18 21:54:23 -0500

    proyecto inicial

---
+++ src/main/java/com/idat/SeptiembreIIIE/service/ProductoServicelmpl.java
@@ -0,0 +1,48 @@
+package com.idat.SeptiembreIIIE.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.idat.SeptiembreIIIE.model.Producto;
+import com.idat.SeptiembreIIIE.repository.ProductoRepository;
+
+
+@Service
+public class ProductoServicelmpl implements ProductoService {
+	
+	@Autowired
+	private ProductoRepository rerpositorio;
+
+	@Override
+	public void guardar(Producto producto) {
+		// TODO Auto-generated method stub
+		rerpositorio.guardar(producto);
+	}
+
+	@Override
+	public void actualizar(Producto producto) {
+		// TODO Auto-generated method stub
+		rerpositorio.actualizar(producto);
+	}
+
+	@Override
+	public void eliminar(Integer id) {
+		// TODO Auto-generated method stub
+		rerpositorio.eliminar(id);
+	}
+
+	@Override
+	public List<Producto> listar() {
+		// TODO Auto-generated method stub
+		return rerpositorio.listar();
+	}
+
+	@Override
+	public Producto obtener(Integer id) {
+		// TODO Auto-generated method stub
+		return rerpositorio.obtener(id);
+	}
+
+}
